5e261bcb8e66e8548b359c3b243052e4871412bb
Xavier Monin <xavier.monin@free.fr>
2016-12-07 16:28:20 +0100

    Indentation correction

--- EntityCore/DynamicEntity/Construction/Helpers/Emitter/EmitHelper.cs
+++ EntityCore/DynamicEntity/Construction/Helpers/Emitter/EmitHelper.cs
@@ -30,11 +30,11 @@ namespace EntityCore.DynamicEntity.Construction.Helpers.Emitter
         }
 
         /// <summary>
-		/// Converts the supplied <see cref="EmitHelper"/> to a <see cref="ILGenerator"/>.
-		/// </summary>
-		/// <param name="emitHelper">The <see cref="EmitHelper"/>.</param>
-		/// <returns>An ILGenerator.</returns>
-		public static implicit operator ILGenerator(EmitHelper emitHelper)
+        /// Converts the supplied <see cref="EmitHelper"/> to a <see cref="ILGenerator"/>.
+        /// </summary>
+        /// <param name="emitHelper">The <see cref="EmitHelper"/>.</param>
+        /// <returns>An ILGenerator.</returns>
+        public static implicit operator ILGenerator(EmitHelper emitHelper)
         {
             if (emitHelper == null)
                 throw new ArgumentNullException("emitHelper");
@@ -45,31 +45,31 @@ namespace EntityCore.DynamicEntity.Construction.Helpers.Emitter
         #region ILGenerator Methods
 
         /// <summary>
-		/// Declares a local variable.
-		/// </summary>
-		/// <param name="localType">The Type of the local variable.</param>
-		/// <returns>The declared local variable.</returns>
-		public LocalBuilder DeclareLocal(Type localType)
+	    /// Declares a local variable.
+	    /// </summary>
+	    /// <param name="localType">The Type of the local variable.</param>
+	    /// <returns>The declared local variable.</returns>
+	    public LocalBuilder DeclareLocal(Type localType)
         {
             return ILGenerator.DeclareLocal(localType);
         }
 
         /// <summary>
-		/// Declares a new label.
-		/// </summary>
-		/// <returns>Returns a new label that can be used as a token for branching.</returns>
-		public Label DefineLabel()
+	    /// Declares a new label.
+	    /// </summary>
+	    /// <returns>Returns a new label that can be used as a token for branching.</returns>
+	    public Label DefineLabel()
         {
             return ILGenerator.DefineLabel();
         }
 
         /// <summary>
-		/// Marks the Microsoft intermediate language (MSIL) stream's current position 
-		/// with the given label.
-		/// </summary>
-		/// <param name="loc">The label for which to set an index.</param>
-		/// <returns>Current instance of the <see cref="EmitHelper"/>.</returns>
-		public EmitHelper MarkLabel(Label loc)
+	    /// Marks the Microsoft intermediate language (MSIL) stream's current position 
+	    /// with the given label.
+	    /// </summary>
+	    /// <param name="loc">The label for which to set an index.</param>
+	    /// <returns>Current instance of the <see cref="EmitHelper"/>.</returns>
+	    public EmitHelper MarkLabel(Label loc)
         {
             ILGenerator.MarkLabel(loc);
             return this;
@@ -93,13 +93,13 @@ namespace EntityCore.DynamicEntity.Construction.Helpers.Emitter
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Ldloc"/>, short) that
-		/// load an argument address onto the evaluation stack.
-		/// </summary>
-		/// <param name="index">Index of the local variable value pushed onto the stack.</param>
-		/// <seealso cref="OpCodes.Ldloc">OpCodes.Ldloc</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,short)">ILGenerator.Emit</seealso>
-		public EmitHelper ldloc(short index)
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Ldloc"/>, short) that
+	    /// load an argument address onto the evaluation stack.
+	    /// </summary>
+	    /// <param name="index">Index of the local variable value pushed onto the stack.</param>
+	    /// <seealso cref="OpCodes.Ldloc">OpCodes.Ldloc</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,short)">ILGenerator.Emit</seealso>
+	    public EmitHelper ldloc(short index)
         {
             ILGenerator.Emit(OpCodes.Ldloc, index);
             return this;
@@ -119,27 +119,27 @@ namespace EntityCore.DynamicEntity.Construction.Helpers.Emitter
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Br_S"/>, label) that
-		/// unconditionally transfers control to a target instruction (short form).
-		/// </summary>
-		/// <param name="label">The label to branch from this location.</param>
-		/// <seealso cref="OpCodes.Br_S">OpCodes.Br_S</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,Label)">ILGenerator.Emit</seealso>
-		public EmitHelper br_s(Label label)
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Br_S"/>, label) that
+	    /// unconditionally transfers control to a target instruction (short form).
+	    /// </summary>
+	    /// <param name="label">The label to branch from this location.</param>
+	    /// <seealso cref="OpCodes.Br_S">OpCodes.Br_S</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,Label)">ILGenerator.Emit</seealso>
+	    public EmitHelper br_s(Label label)
         {
             ILGenerator.Emit(OpCodes.Br_S, label);
             return this;
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Ldarg"/>, short) or 
-		/// ILGenerator.Emit(<see cref="OpCodes.Ldarg_S"/>, byte) that
-		/// loads an argument (referenced by a specified index value) onto the stack.
-		/// </summary>
-		/// <param name="index">Index of the argument that is pushed onto the stack.</param>
-		/// <seealso cref="OpCodes.Ldarg">OpCodes.Ldarg</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,short)">ILGenerator.Emit</seealso>
-		public EmitHelper ldarg(int index)
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Ldarg"/>, short) or 
+	    /// ILGenerator.Emit(<see cref="OpCodes.Ldarg_S"/>, byte) that
+	    /// loads an argument (referenced by a specified index value) onto the stack.
+	    /// </summary>
+	    /// <param name="index">Index of the argument that is pushed onto the stack.</param>
+	    /// <seealso cref="OpCodes.Ldarg">OpCodes.Ldarg</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,short)">ILGenerator.Emit</seealso>
+	    public EmitHelper ldarg(int index)
         {
             switch (index)
             {
@@ -175,52 +175,52 @@ namespace EntityCore.DynamicEntity.Construction.Helpers.Emitter
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Brtrue"/>, label) that
-		/// transfers control to a target instruction if value is true, not null, or non-zero.
-		/// </summary>
-		/// <param name="label">The label to branch from this location.</param>
-		/// <seealso cref="OpCodes.Brtrue">OpCodes.Brtrue</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,Label)">ILGenerator.Emit</seealso>
-		public EmitHelper brtrue(Label label)
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Brtrue"/>, label) that
+	    /// transfers control to a target instruction if value is true, not null, or non-zero.
+	    /// </summary>
+	    /// <param name="label">The label to branch from this location.</param>
+	    /// <seealso cref="OpCodes.Brtrue">OpCodes.Brtrue</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,Label)">ILGenerator.Emit</seealso>
+	    public EmitHelper brtrue(Label label)
         {
             ILGenerator.Emit(OpCodes.Brtrue, label);
             return this;
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Brtrue_S"/>, label) that
-		/// transfers control to a target instruction (short form) if value is true, not null, or non-zero.
-		/// </summary>
-		/// <param name="label">The label to branch from this location.</param>
-		/// <seealso cref="OpCodes.Brtrue_S">OpCodes.Brtrue_S</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,Label)">ILGenerator.Emit</seealso>
-		public EmitHelper brtrue_s(Label label)
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Brtrue_S"/>, label) that
+	    /// transfers control to a target instruction (short form) if value is true, not null, or non-zero.
+	    /// </summary>
+	    /// <param name="label">The label to branch from this location.</param>
+	    /// <seealso cref="OpCodes.Brtrue_S">OpCodes.Brtrue_S</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,Label)">ILGenerator.Emit</seealso>
+	    public EmitHelper brtrue_s(Label label)
         {
             ILGenerator.Emit(OpCodes.Brtrue_S, label);
             return this;
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Newobj"/>, <see cref="ConstructorInfo"/>) that
-		/// creates a new object or a new instance of a value type,
-		/// pushing an object reference (type O) onto the evaluation stack.
-		/// </summary>
-		/// <param name="constructorInfo">A <see cref="ConstructorInfo"/> representing a constructor.</param>
-		/// <seealso cref="OpCodes.Newobj">OpCodes.Newobj</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,ConstructorInfo)">ILGenerator.Emit</seealso>
-		public EmitHelper newobj(ConstructorInfo constructorInfo)
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Newobj"/>, <see cref="ConstructorInfo"/>) that
+	    /// creates a new object or a new instance of a value type,
+	    /// pushing an object reference (type O) onto the evaluation stack.
+	    /// </summary>
+	    /// <param name="constructorInfo">A <see cref="ConstructorInfo"/> representing a constructor.</param>
+	    /// <seealso cref="OpCodes.Newobj">OpCodes.Newobj</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,ConstructorInfo)">ILGenerator.Emit</seealso>
+	    public EmitHelper newobj(ConstructorInfo constructorInfo)
         {
             ILGenerator.Emit(OpCodes.Newobj, constructorInfo);
             return this;
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Ldarg_0"/>) that
-		/// loads the argument at index 0 onto the evaluation stack.
-		/// </summary>
-		/// <seealso cref="OpCodes.Ldarg_0">OpCodes.Ldarg_0</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode)">ILGenerator.Emit</seealso>
-		public EmitHelper ldarg_0()
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Ldarg_0"/>) that
+	    /// loads the argument at index 0 onto the evaluation stack.
+	    /// </summary>
+	    /// <seealso cref="OpCodes.Ldarg_0">OpCodes.Ldarg_0</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode)">ILGenerator.Emit</seealso>
+	    public EmitHelper ldarg_0()
         {
             ILGenerator.Emit(OpCodes.Ldarg_0);
             return this;
@@ -290,31 +290,31 @@ namespace EntityCore.DynamicEntity.Construction.Helpers.Emitter
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Stfld"/>, <see cref="FieldInfo"/>) that
-		/// replaces the value stored in the field of an object reference or pointer with a new value.
-		/// </summary>
-		/// <param name="fieldInfo">A <see cref="FieldInfo"/> representing a field.</param>
-		/// <seealso cref="OpCodes.Stfld">OpCodes.Stfld</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,FieldInfo)">ILGenerator.Emit</seealso>
-		public EmitHelper stfld(FieldInfo fieldInfo)
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Stfld"/>, <see cref="FieldInfo"/>) that
+	    /// replaces the value stored in the field of an object reference or pointer with a new value.
+	    /// </summary>
+	    /// <param name="fieldInfo">A <see cref="FieldInfo"/> representing a field.</param>
+	    /// <seealso cref="OpCodes.Stfld">OpCodes.Stfld</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode,FieldInfo)">ILGenerator.Emit</seealso>
+	    public EmitHelper stfld(FieldInfo fieldInfo)
         {
             ILGenerator.Emit(OpCodes.Stfld, fieldInfo);
             return this;
         }
 
         /// <summary>
-		/// Calls ILGenerator.Emit(<see cref="OpCodes.Ret"/>) that
-		/// returns from the current method, pushing a return value (if present) 
-		/// from the caller's evaluation stack onto the callee's evaluation stack.
-		/// </summary>
-		/// <seealso cref="OpCodes.Ret">OpCodes.Ret</seealso>
-		/// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode)">ILGenerator.Emit</seealso>
-		public EmitHelper ret()
+	    /// Calls ILGenerator.Emit(<see cref="OpCodes.Ret"/>) that
+	    /// returns from the current method, pushing a return value (if present) 
+	    /// from the caller's evaluation stack onto the callee's evaluation stack.
+	    /// </summary>
+	    /// <seealso cref="OpCodes.Ret">OpCodes.Ret</seealso>
+	    /// <seealso cref="System.Reflection.Emit.ILGenerator.Emit(OpCode)">ILGenerator.Emit</seealso>
+	    public EmitHelper ret()
         {
             ILGenerator.Emit(OpCodes.Ret);
             return this;
         }
 
         #endregion
-    }
+}
 }
