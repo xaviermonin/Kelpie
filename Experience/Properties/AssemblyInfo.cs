e2b3a9e3c6ad936be23097276fbe49ac27a15396
Xavier Monin <xavier.monin@free.fr>
2016-12-09 21:45:22 +0100

    Rename Experience to ProtoLab

--- Experience/Properties/AssemblyInfo.cs
+++ Experience/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Experience")]
+[assembly: AssemblyTitle("ProtoLab")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("Xavier Monin")]
 [assembly: AssemblyProduct("Experience")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2016")]
+[assembly: AssemblyCopyright("Copyright © Xavier Monin 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
