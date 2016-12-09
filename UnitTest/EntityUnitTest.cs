8aa2d2a0bf445de81170b09c18e0b4466ea56c7c
Xavier Monin <xavier.monin@free.fr>
2016-12-09 17:04:32 +0100

    Add proxies unit tests.

--- UnitTest/EntityUnitTest.cs
+++ UnitTest/EntityUnitTest.cs
@@ -25,23 +25,105 @@ namespace UnitTest
     [TestClass]
     public class EntityUnitTest
     {
+        private DynamicEntityContext entityContext;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            entityContext = new DynamicEntityContext("name=DataDbContext");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            entityContext.Dispose();
+            entityContext = null;
+        }
+
+        [TestMethod]
+        [TestCategory("Proxy")]
+        public void RetrieveNavigationPropertyWithLazyLoading()
+        {
+            Assert.IsTrue(entityContext.Configuration.LazyLoadingEnabled);
+            var attribute = entityContext.ProxySet<IAttribute>("Attribute").First();
+
+            Assert.IsNotNull(attribute.Entity);
+            Assert.IsInstanceOfType(attribute.Entity, typeof(IEntity));
+
+            Assert.IsNotNull(attribute.Type);
+            Assert.IsNotNull(attribute.Type.Attributes);
+            Assert.IsNotNull(attribute.Type.Attributes.FirstOrDefault());
+
+            Assert.IsNotNull(attribute.Entity.Proxies);
+            Assert.IsNotNull(attribute.Entity.Proxies.FirstOrDefault());
+        }
+
+        [TestMethod]
+        [TestCategory("Proxy")]
+        public void RetrieveSingleProxyEntity()
+        {
+            IAttributeType stringType = entityContext.ProxySet<IAttributeType>("AttributeType")
+                                                     .Where(c => c.ClrName == "System.String")
+                                                     .Single();
+
+            Assert.AreEqual(stringType.ClrName, "System.String");
+            Assert.AreEqual(stringType.SqlServerName, "nvarchar");
+        }
+
+        [TestMethod]
+        [TestCategory("Proxy")]
+        public void RetrieveIncludedProxyEntity()
+        {
+            var attributesUsingStringType = entityContext.ProxySet<IAttributeType>("AttributeType")
+                                                            .Include(c => c.Attributes)
+                                                            .Where(c => c.ClrName == "System.String")
+                                                            .OrderBy(c => c.Id)
+                                                            .SelectMany(c => c.Attributes)
+                                                            .ToArray();
+
+            Assert.IsInstanceOfType(attributesUsingStringType, typeof(IAttribute[]));
+            Assert.AreEqual(attributesUsingStringType.Length, 7);
+            Assert.IsNotNull(attributesUsingStringType.FirstOrDefault());
+        }
+
+        [TestMethod]
+        [TestCategory("Proxy")]
+        public void ManyToOneNavigation()
+        {
+            var attributeName = entityContext.ProxySet<IAttribute>("Attribute")
+                                            .Include(c => c.Entity)
+                                            .Where(c => c.Name == "Name" &&
+                                                        c.Entity.Name == "Entity")
+                                            .Single();
+
+            Assert.AreEqual(attributeName.Name, "Name");
+            Assert.IsNotNull(attributeName.Entity);
+            Assert.AreEqual(attributeName.Entity.Name, "Entity");
+            Assert.IsFalse(attributeName.IsNullable.Value);
+            Assert.IsTrue(attributeName.Entity.Managed.Value);
+        }
+
+        [TestMethod]
+        [TestCategory("Proxy")]
+        public void OneToManyNavigation()
+        {
+            var entity = entityContext.ProxySet<IEntity>("Entity")
+                                        .Include(c => c.Attributes)
+                                        .Where(c => c.Name == "Entity")
+                                        .Single();
+
+            Assert.IsNotNull(entity.Attributes);
+            Assert.AreEqual(entity.Attributes.Count, 5);
+
+            var nameAttribute = entity.Attributes.Where(c => c.Name == "Name")
+                                                    .Single();
+
+            Assert.AreEqual(nameAttribute.Name, "Name");
+        }
+
         [TestMethod]
         public void CreateEntity()
         {
-            DynamicEntityContext entityContext = new DynamicEntityContext("name=DataDbContext");
-            IAttributeType stringType = entityContext.ProxySet<IAttributeType>("AttributeType").Where(c => c.ClrName == "System.String").Single();
-            IAttributeType intType = entityContext.ProxySet<IAttributeType>("AttributeType").Where(c => c.ClrName == "System.Int32").Single();
-
-            var attributesUsingStringType = entityContext.ProxySet<IAttributeType>("AttributeType")
-                                                         .Include(c => c.Attributes)
-                                                         .Where(c => c.ClrName == "System.String")
-                                                         .SelectMany(c => c.Attributes).ToArray();
-
-            foreach (var attribute in attributesUsingStringType)
-                System.Diagnostics.Trace.WriteLine(string.Format("Attribute: {0}. Id: {1}. Type: {2}", attribute.Name, attribute.Id, attribute.Type.ClrName));
-
-            var id = stringType.Id;
-
             /*ICollection<EntityCore.Proxy.IBaseEntity> a = new HashSet<AttributeTest>();
             ICollection<EntityCore.Proxy.Metadata.IAttribute> b = a as ICollection<EntityCore.Proxy.Metadata.IAttribute>;*/
 
