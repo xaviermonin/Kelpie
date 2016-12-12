973093653618fc015d75f0b46cc0fc706d84d32a
Xavier Monin <xavier.monin@free.fr>
2016-12-12 17:58:08 +0100

    Add GetAttributeValue et SetAttribute to IBaseEntity

--- EntityCore.Proxy/IBaseEntity.cs
+++ EntityCore.Proxy/IBaseEntity.cs
@@ -4,5 +4,8 @@ namespace EntityCore.Proxy
     public interface IBaseEntity
     {
         int Id { get; set; }
+
+        T GetAttributeValue<T>(string propertyName);
+        void SetAttributeValue<T>(string propertyName, T value);
     }
 }
